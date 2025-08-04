71ae88231c3c30e855df7df531d44f9cb5aa3c87
subhayu2025 <subhayuchatterjee2022@gmail.com>
2025-08-04 20:45:46 +0530

    added play and pause system

--- script.js
+++ script.js
@@ -22,3 +22,21 @@ document.querySelector("button").addEventListener("click", () =>{
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
+
+
+// button changer //
+
+const btnImg = document.getElementById("btnImg");
+
+const btn = document.getElementById("toggleBtn");
+
+const img1 = 'images/play.png';
+const img2 = 'images/pause.png';
+
+
+let showingFirst = true;
+
+btn.addEventListener("click", () => {
+    btnImg.src = showingFirst ? img2 : img1;
+    showingFirst =!showingFirst;
+});
